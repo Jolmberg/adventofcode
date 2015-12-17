0947bd94b2d4a2f52ef99530a30511429589ca3e
Johannes Holmberg <johannes@update.uu.se>
2015-12-17 15:27:40 +0100

    Solutions for day 13 part 1 and 2

---
+++ 13_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat input13.txt | grep -o "^[A-Z][A-Za-z]*" | uniq | {
+    for x in {a..h}; do
+	read y
+	eval "$x=$y"
+    done
+    for x in {a..h}{a..h}; do
+	eval "$x=\$((\$(cat input13.txt | sed \"s/se / -/g\" | grep \"\$${x:0:1}\" | grep \"\$${x:1:1}\" | grep -o '\-\?[0-9]*' | head -c -1 | tr '\n' '+')))"
+    done
+    for x in {b..h}{b..h}{b..h}{b..h}{b..h}{b..h}{b..h}; do
+	echo $x
+    done | grep "b.*c" | grep d | grep e | grep f | grep g | grep h | while read x; do
+	eval echo "\$((a${x:0:1} + ${x:0:2} + ${x:1:2} + ${x:2:2} + ${x:3:2} + ${x:4:2} + ${x:5:2} + ${x:6:1}a))"
+    done
+} | sort -rn | head -n 1
